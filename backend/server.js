f61b7c3d2d6b9d1de95e46b30c4a22407c3906cc
ivananeskovic <ivananeskovic13@gmail.com>
2026-06-15 00:41:22 +0200

    2. commit

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorHandler.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/api/config/paypal', (req, res) => {
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
