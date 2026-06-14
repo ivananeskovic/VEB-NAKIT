be70d31326d97506bb4eab8b55b816cc2544fcfa
ivananeskovic <ivananeskovic13@gmail.com>
2026-06-14 19:20:54 +0200

    1. commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import dns from 'node:dns/promises';
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorHandler.js';
+import productRoutes from './routes/productRoutes.js';
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.use('/api/products', productRoutes);
+
+app.get('/api/config/paypal', (req, res) => {
+    res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () =>
+    console.log(`Server is running on port ${port}`)
+);
